85c26c48095908c5f8db137c4415e3c4be93b202
Giorgos Komninos <giorgos@tabeo.co.uk>
2021-09-15 08:46:34 +0300

    Implements /healthz and /metrics endpoints

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,12 +1,30 @@
+import threading
 import sqlite3
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
+class AtomicCounter():
+
+    def __init__(self, value=0):
+        self._value = value
+        self.lock = threading.Lock()
+
+    def inc(self, d=1):
+        with self.lock:
+            self._value += 1
+    @property
+    def value(self):
+        with self.lock:
+            return self._value
+
+connection_counter = AtomicCounter()
+
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
     connection = sqlite3.connect('database.db')
+    connection_counter.inc(1)
     connection.row_factory = sqlite3.Row
     return connection
 
@@ -22,7 +40,7 @@ def get_post(post_id):
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
-# Define the main route of the web application 
+# Define the main route of the web application
 @app.route('/')
 def index():
     connection = get_db_connection()
@@ -30,7 +48,7 @@ def index():
     connection.close()
     return render_template('index.html', posts=posts)
 
-# Define how each individual article is rendered 
+# Define how each individual article is rendered
 # If the post ID is not found a 404 page is shown
 @app.route('/<int:post_id>')
 def post(post_id):
@@ -45,7 +63,7 @@ def post(post_id):
 def about():
     return render_template('about.html')
 
-# Define the post creation functionality 
+# Define the post creation functionality
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
@@ -64,6 +82,20 @@ def create():
             return redirect(url_for('index'))
 
     return render_template('create.html')
+
+# Healthcheck endpoint
+@app.route('/healthz', methods=('GET', ))
+def healthz():
+    return {"result": "OK - healthy"}
+
+# Metrics endpoint
+@app.route('/metrics', methods=('GET', ))
+def metrics():
+    with get_db_connection() as connection:
+        post_count = connection.execute("SELECT COUNT(1) FROM posts").fetchone()
+    return {"db_connection_count": connection_counter.value,
+            "post_count": post_count[0]}
+
 
 # start the application on port 3111
 if __name__ == "__main__":
